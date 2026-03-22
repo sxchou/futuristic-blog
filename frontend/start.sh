d7e287867d3a44db833a82bd8fe48f0cb99e84a9
zhouzhouya <228373465@qq.com>
2026-03-22 18:27:52 +0800

    fix: 恢复 nginx 默认配置

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 PORT=${PORT:-80}
-BACKEND_URL=${BACKEND_URL:-"http://backend.railway.internal:8000"}
 
 sed -i "s/listen 80/listen $PORT/g" /etc/nginx/conf.d/default.conf
-sed -i "s|{{BACKEND_URL}}|$BACKEND_URL|g" /etc/nginx/conf.d/default.conf
 
 echo "Starting nginx on port $PORT..."
-echo "Backend URL: $BACKEND_URL"
 
 exec nginx -g "daemon off;"
