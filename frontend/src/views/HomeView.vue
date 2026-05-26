8106191c19f1a2fd1ea0d0fa917f44a84125ac52
zhouzhouya <228373465@qq.com>
2026-05-26 20:06:05 +0800

    style: 移除轮播组件圆角

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -332,7 +332,7 @@ const handlePageChange = (page: number) => {
           class="mb-6"
         >
           <div 
-            class="glass-card select-none"
+            class="glass-card rounded-none select-none"
             @touchstart="handleTouchStart"
             @touchmove="handleTouchMove"
             @touchend="handleTouchEnd"
